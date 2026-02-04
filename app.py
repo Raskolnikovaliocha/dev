92f910a8859120116222b93a0b9f4d766c8660d4
Raskolnikovaliocha <jose.g.oliveira@ufv.br>
2026-02-04 14:56:52 -0300

    Change dropna to remove columns with NA values

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ with tab1:
                             st.write('Dados com valores médios substituidos no lugar de NA')
                             st.dataframe(data)
                         else:
-                            data = data.dropna(axis=0)
+                            data = data.dropna(axis=1)
                             st.dataframe(data)  # manter o mes
 
 
